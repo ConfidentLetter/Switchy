d85dc9afb0e5e2d74603c381079958f93e452000
ConfidentLetter <71581547+ConfidentLetter@users.noreply.github.com>
2025-08-14 20:16:16 -0700

    Initial Commit

---
+++ SwitchyTests/SwitchyTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwitchyTests.swift
+//  SwitchyTests
+//
+//  Created by Cayden M. Ching on 8/14/25.
+//
+
+import Testing
+@testable import Switchy
+
+struct SwitchyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
